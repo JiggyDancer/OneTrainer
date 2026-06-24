49b600436465a7c370164c8421f6a8f0294dc130
JiggyDancer <67120186+JiggyDancer@users.noreply.github.com>
2026-06-24 10:12:39 -0400

    Update lr_scheduler_util.py

--- modules/util/lr_scheduler_util.py
+++ modules/util/lr_scheduler_util.py
@@ -53,7 +53,7 @@ def lr_lambda_cosine_with_restarts(
 ):
     def lr_lambda(current_step: int):
         progress = float(current_step - 1) / float(scheduler_steps)
-        cos_val = 0.5 * (1.0 + math.cos((2.0 * num_cycles + 1.0) * progress * math.pi))
+        cos_val = 0.5 * (1.0 + math.cos(2.0 * num_cycles * progress * math.pi))
         factor = max(0.0, cos_val)
         factor = apply_min_factor(factor, min_factor)
         return factor
